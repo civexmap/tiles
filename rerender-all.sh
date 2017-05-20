e81d0851471461a7360bc7dc5b66cd5bb1d85d28
Gjum <code.gjum@gmail.com>
2017-05-21 01:53:56 +0200

    update tiles
    used jecowa's full world cache and his prerendered biome+terrain tiles

---
+++ rerender-all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname $BASH_SOURCE)"
+
+cache="/home/gjum/data/civex3-voxelmap/cache/world_latest/"
+rustmap_install="/home/gjum/src/mc-misc/rustmap/"
+render="$rustmap_install/target/release/render"
+
+python3 "$rustmap_install/py/cleanup.py" terrain/z0 -f
+# python3 "$rustmap_install/py/zoom.py" terrain -6
+
+# for tileset in simple biome height light; do
+for tileset in simple height light; do
+    "$render" "$cache" $tileset/z0/'{tile}.png' $tileset
+    python3 "$rustmap_install/py/cleanup.py" $tileset/z0 -f
+    python3 "$rustmap_install/py/zoom.py" $tileset -6
+done
+
+python3 "$rustmap_install/py/build_night.py" night/z0 terrain/z0 light/z0
+python3 "$rustmap_install/py/zoom.py" night -6
